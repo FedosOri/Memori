24303a27cd37b4fa8eac8ae02feb0513ad404e26
FedosOri <fedorsmolin2006@gmail.com>
2022-04-30 12:51:14 +0300

    добавил обработку команд

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import telebot
 from telebot import types
-from data import config
-
+from settings import config
 
 bot = telebot.TeleBot(config.token)
+
+new_note_step_1 = False
+new_reminder_step_1 = False
+new_reminder_step_2 = False
 
 
 @bot.message_handler(commands=["start", "menu"])
 def start(message):
-    mes = "Привет"
+    mes = "привет"
     bot.send_message(message.chat.id, mes, reply_markup=create_default_keyboard())
 
 
@@ -27,9 +30,22 @@ def notes(message):
 
 @bot.message_handler(commands=["new_note"])
 def new_note(message):
-    # создать новую заметку
-    mes = "новая заметка создана"
-    bot.send_message(message.chat.id, mes)
+    global new_note_step_1
+    new_note_step_1 = True
+    mes = "введите содержание заметки:"
+    bot.send_message(message.chat.id, mes, reply_markup=create_special_keyboard(["отмена"]))
+
+
+@bot.message_handler(func=lambda message: new_note_step_1, content_types=["text"])
+def create_new_note(message):
+    global new_note_step_1
+    new_note_step_1 = False
+    if message.text == "отмена":
+        mes = "отмена создания"
+    else:
+        # создать новую заметку
+        mes = "заметка создана"
+    bot.send_message(message.chat.id, mes, reply_markup=create_default_keyboard())
 
 
 @bot.message_handler(commands=["reminders"])
@@ -41,8 +57,36 @@ def reminders(message):
 
 @bot.message_handler(commands=["new_reminder"])
 def new_reminder(message):
-    # создать новое напоминание
-    mes = "новое напоминание создано"
+    global new_reminder_step_1
+    new_reminder_step_1 = True
+    mes = "введите, что вам напомнить:"
+    bot.send_message(message.chat.id, mes, reply_markup=create_special_keyboard(["отмена"]))
+
+
+@bot.message_handler(func=lambda message: new_reminder_step_1, content_types=["text"])
+def create_new_reminder_step_1(message):
+    global new_reminder_step_1, new_reminder_step_2
+    if message.text == "отмена":
+        mes = "отмена создания"
+    else:
+        new_reminder_step_2 = True
+        mes = "введите, когда вам напомнить:"
+    new_reminder_step_1 = False
+    bot.send_message(message.chat.id, mes, reply_markup=create_special_keyboard(["назад", "отмена"]))
+
+
+@bot.message_handler(func=lambda message: new_reminder_step_2, content_types=["text"])
+def create_new_reminder_step_1(message):
+    global new_reminder_step_1, new_reminder_step_2
+    if message.text == "отмена":
+        mes = "отмена создания"
+    elif message.text == "назад":
+        new_reminder_step_1 = True
+        mes = "введите, что вам напомнить:"
+    else:
+        # создать напоминание
+        mes = "напоминание создано"
+    new_reminder_step_2 = False
     bot.send_message(message.chat.id, mes)
 
 
@@ -63,10 +107,17 @@ def create_default_keyboard():
     item1 = types.KeyboardButton("/notes")
     item2 = types.KeyboardButton("/new_note")
     item3 = types.KeyboardButton("/reminders")
-    item4 = types.KeyboardButton("/new_reminders")
+    item4 = types.KeyboardButton("/new_reminder")
     item5 = types.KeyboardButton("/menu")
     item6 = types.KeyboardButton("/help")
     keyboard.add(item1, item2, item3, item4, item5, item6)
+    return keyboard
+
+
+def create_special_keyboard(items):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for item in items:
+        keyboard.add(types.KeyboardButton(item))
     return keyboard
 
 
